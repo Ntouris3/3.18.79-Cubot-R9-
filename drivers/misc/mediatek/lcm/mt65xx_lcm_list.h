d72941fc7c79bd8a9cba2c3d3137616809aa58ff
Ruben Espinola <rubenes2003@gmail.com>
2020-03-30 11:31:21 +0200

    Update mt65xx_lcm_list.h

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
@@ -2,7 +2,7 @@
 #define __MT65XX_LCM_LIST_H__
 
 #include <lcm_drv.h>
-extern LCM_DRIVER ili9881c_cpt50_haifei_hd_drv;
+extern LCM_DRIVER ili9881c_cpt50_haifei_hd_lcm_drv;
 #ifdef BUILD_LK
 extern void mdelay(unsigned long msec);
 #endif
